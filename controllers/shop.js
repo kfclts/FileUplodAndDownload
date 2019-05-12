83131b73108f918dacc56b74fc57b10d9b429451
KFC <kfclts@gmail.com>
2019-05-12 20:00:39 +0800

    Restricting File Access

--- controllers/shop.js
+++ controllers/shop.js
@@ -152,6 +152,14 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) {
+        return next(new Error('No order found.'));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error('Unauthorized'));
+      }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
       fs.readFile(invoicePath, (err, data) => {
@@ -165,4 +173,6 @@ exports.getInvoice = (req, res, next) => {
         );
         res.send(data);
       });
+    })
+    .catch(err => next(err));
 };
